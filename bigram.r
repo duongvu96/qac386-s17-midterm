09f38e45a653dec22fc9fdbdbbcdbb36def93ab2
Duong Vu <tvunguyen@wesleyan.edu>
2017-04-22 21:24:20 -0400

    Saved bigram.r

--- bigram.r
+++ bigram.r
@@ -7,15 +7,15 @@ argusData = read.csv('./js-server/public/argus_civil_engagement.csv')
 argusDataStr = paste(argusData$content, collapse=" ")
 
 groupData = read.csv('./js-server/public/group_responses.csv')
-groupDataStr = paste(groupData$phrase, collapse=" ")
+groupDataStr = paste(groupData$Answer1, collapse=" ")
 
-process_bigrams <- function(s, filename) {
+process_bigrams <- function(str) {
   str = gsub("[0-9]", "", str)
   str = gsub("[[:punct:]]", "", str)
   str = gsub("  ", " ", str)
   str = tolower(str)
   
-  word_vec = strsplit(argusDataStr, split=" ")[[1]]
+  word_vec = strsplit(str, split=" ")[[1]]
   word_vec = word_vec[!word_vec %in% stopwords('en')]
   
   ## remove empty entries (arising from spaces)
@@ -25,5 +25,8 @@ process_bigrams <- function(s, filename) {
   x = ngrams(word_vec, n=2)
   bigrams = unlist(sapply(x, paste, collapse=" "))
   bigrams_df = data.frame(Phrases=bigrams, stringsAsFactors = F)
-  write.csv(bigrams_df, paste(c(filename, "_bigrams.csv"), collapse = ""), row.names=F)
+  return(bigrams_df)
 }
+
+write.csv(process_bigrams(argusDataStr), "argus_bigrams.csv", row.names=F)
+write.csv(process_bigrams(groupDataStr), "group_bigrams.csv", row.names=F)
